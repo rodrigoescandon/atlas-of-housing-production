7b978d69c36b36617454e4fb24f7e5dd077772ec
Rodrigo Escandón Cesarman <rodrigo.escandon.cesarman@gmail.com>
2020-08-09 22:59:07 -0500

    Testing modal–about to install Spruce.

---
+++ pages/ideas.md
@@ -0,0 +1,11 @@
+---
+title: Ideas for the archive
+date: 2017-01-01T00:00:00.000Z
+permalink: false
+# eleventyNavigation:
+#   key: About
+#   order: 1
+---
+
+- What is appropriate housing ("vivienda digna y decorosa")?
+- 
--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     },
   },
   theme: {
+    maxHeight: {
+      "0": "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
     extend: {
       gridTemplateRows: {
         layout: "auto 1fr auto",
